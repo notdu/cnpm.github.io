28c9a093443749ccbc528af1eb5c267251f75e84
huudutg <huudutg@gmail.com>
2019-12-23 19:10:38 +0700

    create project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs_sections = require('express-handlebars-sections');
+const morgan = require('morgan');
+const numeral = require('numeral');
+require('express-async-errors');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
+
+
+
+// app.use(require('./middlewares/locals.mdw'));
+
+require('./middlewares/locals.mdw')(app);
+require('./middlewares/routes.mdw')(app);
+
+app.use((req, res, next) => {
+  // res.render('vwError/404');
+  res.send('You\'re lost');
+})
+
+//
+// default error handler
+
+app.use((err, req, res, next) => {
+  // res.render('vwError/index');
+  console.error(err.stack);
+  res.status(500).send('View error on console.');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+})
